ff4a297ba0216342ea6722cd7412932e1c8fc1da
Graphic <graphic@SOD-SJH-516-09M.local>
2015-11-06 14:39:00 -0500

    third

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 var resizesq= function(){
 	$(this).height($(this).width())
+
+	var imgheight=$("img").height();
+	var imgwidth=$("img").width();
+
 }
 
 $(".galimg").each(resizesq);
 
-if($(".galimg").width<$(".galimg").height){
-	$(this).addClass("landscape");
-}
+// if($(".galimg").width<$(".galimg").height){
+// 	$(this).addClass("landscape");
+// }
+
+
+	if (imgheight>imgwidth) {
+		$("img").addClass("portrait");
+	}
+	else if (imgwidth> imgheight){
+		$("img").addClass("landscape");
+	}
