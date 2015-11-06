886916b698f4c6e67b00cc8b3f5aa6c050f9f4c0
Graphic <graphic@SOD-SJH-516-09M.local>
2015-11-06 14:31:54 -0500

    squares

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-Math
+var resizesq= function(){
+	$(this).height($(this).width())
+}
+
+$(".galimg").each(resizesq);
+
+if($(".galimg").width<$(".galimg").height){
+	$(this).addClass("landscape");
+}
